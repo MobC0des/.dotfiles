c0da8477c380528fabc9b89dff46fd4e493ca5c8
Scott <scott.brown@blendb2b.com>
2026-06-26 16:24:18 +0100

    feat: auto-install hubspot cli on new machine

--- home/.config/fish/conf.d/npm-globals.fish
+++ home/.config/fish/conf.d/npm-globals.fish
@@ -1,2 +1,8 @@
 # Global npm packages - installed on first run
 # To reinstall: npm install -g @hubspot/cli
+#
+# Auto-install global npm packages if not present
+if not command -q hs
+    echo "📦 Installing @hubspot/cli..."
+    npm install -g @hubspot/cli
+end
